19a9f6ca69d4b9c516ab1266704095bece238fbd
Raavi Kaur <raavi211299@gmail.com>
2024-09-28 22:35:30 -0500

    add code of morse code to string conversion with condition

--- main.py
+++ main.py
@@ -8,10 +8,21 @@ morse_dict = {
     ':': '---...', ';': '-.-.-.', '=': '-...-', '+': '.-.-.', '-': '-....-', '_': '..--.-', '"': '.-..-.',
     '$': '...-..-', '@': '.--.-.', ' ': '/'
 }
-
-message = input('Enter your message want to convert to morse: ')
-morse_code = ''
-for char in message.upper():
-    if char in morse_dict:
-        morse_code += morse_dict[char] + ' '
-print(morse_code)
+user_choice = input('Choose an option: 1 Convert string to morse 2: Convert morse to string and 0: to quit\n')
+if user_choice == '1':
+    message = input('Enter your message want to convert to morse: ')
+    morse_code = ''
+    for char in message.upper():
+        if char in morse_dict:
+            morse_code += morse_dict[char] + ' '
+    print(morse_code)
+elif user_choice == '2':
+    morse_code = input('Enter your morse message want to convert to string')
+    string_message = ''
+    for char in morse_code.split():
+        for ch, value in morse_dict.items():
+            if value == char:
+                string_message += ch
+    print(string_message)
+else:
+    print('invalid input')
